4a82b66aa979acff15c12bc55716841bec5a5f2a
Brian Lajoie <aashmire@gmail.com>
2025-03-12 19:09:25 -0400

    Add files via upload

--- github stuff/strings321.js
+++ github stuff/strings321.js
@@ -1,31 +1,32 @@
 function strings321()
 {
+    //Variables for the palindrome checker
     var string1 = document.getElementById("firstString").value;
     var string2 = document.getElementById("secondString").value;
-    var str3 = string1 + string2;
-    //document.getElementById("updates").innerHTML= "Concatenated string = " + str3;
-
-    var len = str3.length;
-    //document.getElementById("updates").innerHTML= "len of concat string =" + len;
-
-    var substring = str3.substring(1,3)
-    //document.getElementById("updates").innerHTML="substring of str3 (1,3) = " + substring;
-    var lowerC = str3.toLowerCase();
-    lowerC = lowerC.replace(/qwe/g, "asd");
-    //var repBL = str3.replace(/qwe/g, "asd");
-    //document.getElementById("updates").innerHTML="replace qwe with asd" + repBL;
-    document.getElementById("updates").innerHTML="replace qwe with asd" + lowerC;
-
-    //palindrome checker
+    //Here is the code for the palindrome checker
     var splitstr = string1.split("");
     var reversestr = splitstr.reverse("");
     var joinstr = reversestr.join("");
+    //Conditions
     if (string1 == joinstr)
     {
         document.getElementById("updates").innerHTML = "You're A Palindrome!";
     }
     else
     {
-        document.getElementById("updates").innerHTML = "You Are NOT A Palindrome!"
+        document.getElementById("updates").innerHTML = "You Are NOT A Palindrome!";
+    }
+    //Here is the code I added to ask the user for input in string 2
+    if (string2.length == 0)
+    {
+        document.getElementById("Play Again?").innerHTML = "Enter String 2!";
+    }
+    else if (string2 == joinstr)
+    {
+        document.getElementById("Play Again?").innerHTML = "You're A Palindrome!";
+    }
+    else 
+    {
+        document.getElementById("Play Again?").innerHTML = "You Are NOT A Palindrome!";
     }
 }
