4f7425921ebc3f5bb8a6ecb4d76165d38476f5ee
Brian Lajoie <aashmire@gmail.com>
2025-03-22 15:28:14 -0400

    Add files via upload

--- github stuff/Sounds.js
+++ github stuff/Sounds.js
@@ -1,16 +1,30 @@
-function play()
+//Took me a few to figure out how to program 4 buttons.
+//Button functions for coin sound
+function play1()
 {
     mySound = new sound("coin-257878.mp3");
     mySound.play();
-    document.getElementById("Click For Sound").disabled = true;
-    document.getElementById("Click to Mute").disabled = false;
+    document.getElementById("Click For Coin").disabled = true;
+    document.getElementById("Click For No Coin").disabled = false;
 }
 
-function stop()
+function stop1()
 {
     window.location.reload();
 }
+//Button functions for mario death sound
+function play2()
+{
+    mySound = new sound("mario death.mp3");
+    mySound.play();
+    document.getElementById("Click For Death").disabled = true;
+    document.getElementById("Click For No Death").disabled = false;
+}
 
+function stop2()
+{
+    window.location.reload();
+}
 function sound(src)
 {   // add play function
     this.sound = document.createElement("audio") ;//create audio
